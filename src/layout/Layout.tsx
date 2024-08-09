819990890c75ba116f0fcc5b7b55ffa95a37cbd6
Michal Grzesiak <darkjjookkeerr@gmail.com>
2024-08-09 20:23:34 +0200

    add ThemeProvider to layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from '@mui/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from 'src/theme/theme';
 
