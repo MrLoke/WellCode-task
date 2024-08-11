259e8b0d8f8e61c4f747e79f1579d3577a73e920
Michal Grzesiak <darkjjookkeerr@gmail.com>
2024-08-11 13:21:03 +0200

    style not found page

--- src/pages/NotFoundPage/NotFound.tsx
+++ src/pages/NotFoundPage/NotFound.tsx
@@ -1,7 +1,20 @@
+import { Box, Link } from '@mui/material';
+import { routesPath } from 'src/routes/routes';
+
 export const NotFound = () => {
   return (
-    <div>
-      <h1>NotFound</h1>
-    </div>
+    <Box
+      sx={{
+        mt: '5rem',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <h1>Page Not Found 😵</h1>
+      <Link href={routesPath.HOME} color="inherit">
+        Back to home
+      </Link>
+    </Box>
   );
 };
